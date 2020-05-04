55d461460845e411025db819f90e98b7a9d3aef0
n0513n <n3150n@outlook.com>
2020-05-03 23:57:17 -0300

    added file parameters

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Converts file to and from .CSV/.XLS(X) extension formats.
 Requires **xlrd** and **xlwt** libraries (available from PyPI).
 
 ```
-usage: xls2csv.py [-h] [-o OUTPUT] [--quoting QUOTING] input
+usage: xls2csv.py [-h] [-o OUTPUT] [-q {0,1,2,3}] [-e ENCODING] input
 
 positional arguments:
   input                 input file name
@@ -15,5 +15,9 @@ optional arguments:
   -h, --help            show this help message and exit
   -o OUTPUT, --output OUTPUT
                         output file or folder name
-  --quoting QUOTING     text field delimiter (defaut: minimal)
+  -q {0,1,2,3}, --quoting {0,1,2,3}
+                        text quoting {0: 'minimal', 1: 'all',
+                        2: 'non-numeric', 3: 'none'}
+  -e ENCODING, --encoding ENCODING
+                        file encoding (default: utf-8)
 ```
--- xls2csv.py
+++ xls2csv.py
@@ -4,7 +4,7 @@
 '''
 Converts file to and from CSV/XLS/XLX.
 
-usage: xls2csv.py [-h] [-o OUTPUT] [--quoting QUOTING]
+usage: xls2csv.py [-h] [-o OUTPUT] [-q {0,1,2,3}] [-e ENCODING]
                   input
 
 positional arguments:
@@ -14,7 +14,11 @@ optional arguments:
   -h, --help            show this help message and exit
   -o OUTPUT, --output OUTPUT
                         output file or folder name
-  --quoting QUOTING     text field delimiter
+  -q {0,1,2,3}, --quoting {0,1,2,3}
+                        text quoting {0: 'minimal', 1: 'all',
+                        2: 'non-numeric', 3: 'none'}
+  -e ENCODING, --encoding ENCODING
+                        file encoding (default: utf-8)
 '''
 
 from argparse import ArgumentParser
@@ -26,8 +30,15 @@ from os.path import exists, isdir, isfile
 from xlrd import open_workbook
 from xlwt import Workbook
 
-def convert_file(input_name,
-    output_name=None, quoting=QUOTE_MINIMAL):
+ENCODING = 'utf-8'
+
+QUOTING = {0: 'minimal',
+           1: 'all',
+           2: 'non-numeric',
+           3: 'none'}
+
+def convert_file(input_name, output_name=None,
+    quoting=QUOTE_MINIMAL, encoding=ENCODING):
     '''
     Converts file based on extension format.
     '''
@@ -35,12 +46,12 @@ def convert_file(input_name,
 
     if ext in ('.xls', '.xlsx'):
         # convert to CSV file
-        xls2csv(input_name, output_name, quoting)
+        xls2csv(input_name, output_name, quoting, encoding)
     else: # convert to Excel file
-        csv2xls(input_name, output_name, quoting)
+        csv2xls(input_name, output_name, quoting, encoding)
 
 def csv2xls(input_name, output_file=None,
-    delimiter=None, quoting=QUOTE_MINIMAL):
+    quoting=QUOTE_MINIMAL, encoding=ENCODING):
     '''
     Converts CSV format file to Excel (XLS).
     '''
@@ -68,10 +79,10 @@ def csv2xls(input_name, output_file=None,
     book = Workbook()
 
     for s,n in enumerate(input_files):
-        delimiter = get_file_delimiter(n)
+        delimiter = get_file_delimiter(n, encoding)
         sheet = book.add_sheet(basename(n))
 
-        with open(n, 'r') as f:
+        with open(n, 'r', encoding=encoding) as f:
             file_reader = reader(f, delimiter=delimiter, quoting=quoting)
             header = next(file_reader)
             row = sheet.row(0)
@@ -85,7 +96,7 @@ def csv2xls(input_name, output_file=None,
     book.save(output_file)
 
 def xls2csv(input_file, output_folder='.',
-    delimiter=None, quoting=QUOTE_MINIMAL):
+    delimiter=None, quoting=QUOTE_MINIMAL, encoding=ENCODING):
     '''
     Converts Excel (xls/xlsx) format files to CSV.
     '''
@@ -111,7 +122,7 @@ def xls2csv(input_file, output_folder='.',
     for i in sheets:
         s = input_xls.sheet_by_name(i)
         o = output_folder+'/'+name+'_'+str(i)+'.csv'
-        with open(o, 'w') as f:
+        with open(o, 'w', encoding=encoding) as f:
             w = writer(f, delimiter=delimiter, quoting=quoting)
             for line in range(s.nrows):
                 row = s.row_values(line)
@@ -120,11 +131,11 @@ def xls2csv(input_file, output_folder='.',
 
     return lst
 
-def get_file_delimiter(input_name):
+def get_file_delimiter(input_name, encoding=ENCODING):
     '''
     Returns character delimiter from file.
     '''
-    with open(input_name, 'rt', encoding='utf8') as input_file:
+    with open(input_name, 'rt', encoding=encoding) as input_file:
         file_reader = reader(input_file)
         header = str(next(file_reader))
 
@@ -140,10 +151,12 @@ if __name__ == "__main__":
 
     parser.add_argument('input', action='store', help='input file name')
     parser.add_argument('-o', '--output', action='store', help='output file or folder name')
-    parser.add_argument('--quoting', action='store', help='text field delimiter (defaut: minimal)', default=QUOTE_MINIMAL)
+    parser.add_argument('-q', '--quoting', action='store', type=int, choices=QUOTING.keys(), default=0, help='text quoting %s' % QUOTING)
+    parser.add_argument('-e', '--encoding', action='store', help='file encoding (default: %s)' % ENCODING)
 
     args = parser.parse_args()
 
     convert_file(args.input,
                  args.output,
-                 args.quoting)
+                 args.quoting,
+                 args.encoding)
